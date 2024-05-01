2408f8f00fddc6e40c9e461973d8e021a12be5c5
Senu-sesi2 <senu.sesi2@gmail.com>
2024-05-01 08:47:15 +0000

    file storage updates

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,7 +77,7 @@ class DBStorage:
 
     def get(self, cls, id):
         """
-        Returns the object based on the class and its ID, 
+        Returns the object based on the class and its ID,
         or None if not found
         """
         if cls not in classes.values():
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -69,7 +69,8 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
-def get(self, cls, id):
+
+    def get(self, cls, id):
         """
         Returns the object based on the class name and its ID, or None if not found
         """
